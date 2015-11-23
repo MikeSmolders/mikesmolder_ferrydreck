8d9c052e2bd9ad813f4c15129dcd2c5c89b1c483
Dennis van Rooij <dvrooij@ictmbo.nl>
2015-11-23 14:30:01 +0100

    index gemaakt

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Mike und Dennis</title>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <p>Hoi!!!!!</p>
+    </body>
+</html>
